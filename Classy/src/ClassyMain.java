061908e1c63e974cd4d77dd95527ab33fb9b69bd
nicholas hays <nicholas.hays@hotmail.com>
2016-07-06 14:57:31 -0700

    handled input from stdin

--- Classy/src/ClassyMain.java
+++ Classy/src/ClassyMain.java
@@ -1,9 +1,39 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
 
 public class ClassyMain {
-
+	static List<Person> thePeople = new ArrayList<Person>();
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner scanIn = new Scanner(System.in);
+		int numOfPeople = scanIn.nextInt();
+		
+		for (int i = 0; i < numOfPeople; i++) {
+			String name = scanIn.next(":");
+			StringBuilder sb = new StringBuilder();
+			while(!scanIn.hasNext("class")) {
+				int classToInt;
+				String theClass = scanIn.next();
+				if(theClass.equals("upper")) classToInt = 3;
+				else if(theClass.equals("middle"))	classToInt = 2;
+				else classToInt = 1;
+				sb.append(classToInt);
+			}
+			thePeople.add(new Person(name, sb));
+		}
+		
+		
+	
+		Collections.sort(thePeople, new ClassComparator());
+		
+		for(Person p : thePeople) {
+			System.out.println(p.myName + " " + p.myClass.toString());
+		}
+		
+		
+		
 	}
-
+	
 }
