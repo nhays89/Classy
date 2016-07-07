4428e796912145127edfc65f0c6ef208f1b8d66b
nicholas hays <nicholas.hays@hotmail.com>
2016-07-06 20:45:52 -0700

    commented Person.

--- Classy/src/Person.java
+++ Classy/src/Person.java
@@ -2,9 +2,28 @@
  * @version: 1.0
  * @author Nicholas Hays
  **/
+
+/**
+ * Represents a Person that holds a class and name.
+ * @author Nicholas A. Hays
+ */
 public class Person {
+	/**
+	 * Name.
+	 */
 	String myName;
+	
+	/**
+	 * Class.
+	 */
 	StringBuilder myClass;
+	
+	/**
+	 * Constructor that sets initial values.
+	 * 
+	 * @param theName the name of the person.
+	 * @param theClass the class of the person. 
+	 */
 	public Person(String theName, StringBuilder theClass) {
 		myName = theName;
 		myClass = theClass;
