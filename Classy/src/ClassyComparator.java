1ffb5a43e0a3c299f4b8a99d24809f3a00ba4f4f
nicholas hays <nicholas.hays@hotmail.com>
2016-07-06 20:43:58 -0700

    tidied up code.

--- Classy/src/ClassyComparator.java
+++ Classy/src/ClassyComparator.java
@@ -1,25 +1,28 @@
+
 /**
  * @version: 1.0
  * @author Nicholas Hays
  */
 import java.util.Comparator;
 
-
-public class ClassyComparator implements Comparator<Person>{
+/**
+ * Compares Person objects on class.
+ * 
+ * @author Nicholas A. Hays
+ */
+public class ClassyComparator implements Comparator<Person> {
 
 	@Override
 	public int compare(Person p1, Person p2) {
-		int classCompare = compareClasses(p1,p2);
-		System.out.println("p1 name: " + p1.myName + "; p2Name " + p2.myName + " classCompare: " + classCompare);
-		
-		if(classCompare < 0) {
+		int classCompare = compareClasses(p1, p2);
+		if (classCompare < 0) {
 			return 1;
 		} else if (classCompare == 0) {
 			int nameCompare = p1.myName.compareTo(p2.myName);
-			if(nameCompare < 0) {
-				return 1;
-			} else {
+			if (nameCompare < 0) {
 				return -1;
+			} else {
+				return 1;
 			}
 		} else {
 			return -1;
@@ -27,19 +30,18 @@ public class ClassyComparator implements Comparator<Person>{
 	}
 
 	private int compareClasses(Person p1, Person p2) {
+
 		StringBuilder p1Temp = new StringBuilder(p1.myClass.toString());
 		StringBuilder p2Temp = new StringBuilder(p2.myClass.toString());
-		System.out.println("p1" + p1.myName);
-		System.out.println("p2" + p2.myName);
 		int p1Length = p1Temp.length();
 		int p2Length = p2Temp.length();
 		int difference = Math.abs(p1Length - p2Length);
-		if(p1Length > p2Length) {
-			for(int i = 0; i < difference; i++) {
+		if (p1Length > p2Length) {
+			for (int i = 0; i < difference; i++) {
 				p2Temp.append(2);
 			}
-		} else if(p1Length < p2Length) {
-			for(int i = 0; i < difference; i++) {
+		} else if (p1Length < p2Length) {
+			for (int i = 0; i < difference; i++) {
 				p1Temp.append(2);
 			}
 		}
--- Classy/src/ClassyMain.java
+++ Classy/src/ClassyMain.java
@@ -1,32 +1,51 @@
+
 /**
  * @version: 1.0
  * @author Nicholas Hays
- *
  **/
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
-import java.util.regex.Pattern;
 
 public class ClassyMain {
+
+	/**
+	 * List of Person objects.
+	 */
 	static List<Person> thePeople = new ArrayList<Person>();
+
+	/**
+	 * Accepts piped input from file and orders people on their 'class'. 'Class'
+	 * here denotes rank in society. For instance, 'upper' class individuals are
+	 * ranked higher than 'lower' class individuals. Input from file is
+	 * converted to integers representing class heiarchy (i.e John: upper middle
+	 * upper lower class = 3231); This integer is then reversed to start the
+	 * comparison of ascii values at the low index because charAt(index) begins
+	 * at 0.
+	 * 
+	 * @param args
+	 *            no cmd line args. Only piped file input.
+	 */
 	public static void main(String[] args) {
-		
+
 		Scanner scanIn = new Scanner(System.in);
 		int numOfPeople = scanIn.nextInt();
-		scanIn.findInLine(Pattern.compile(":"));
+
 		for (int i = 0; i < numOfPeople; i++) {
-			
 			String name = scanIn.next();
-			name = name.substring(0, name.length() -1);
+			name = name.substring(0, name.length() - 1);
 			StringBuilder sb = new StringBuilder();
-			while(!scanIn.hasNext("class")) {
+
+			while (!scanIn.hasNext("class")) {
 				int classToInt;
 				String theClass = scanIn.next();
-				if(theClass.equals("upper")) classToInt = 3;
-				else if(theClass.equals("middle"))	classToInt = 2;
-				else classToInt = 1;
+				if (theClass.equals("upper"))
+					classToInt = 3;
+				else if (theClass.equals("middle"))
+					classToInt = 2;
+				else
+					classToInt = 1;
 				sb.append(classToInt);
 			}
 			scanIn.next();
@@ -35,19 +54,10 @@ public class ClassyMain {
 		}
 
 		scanIn.close();
-		for(int i = 0; i < thePeople.size(); i++) {
+		Collections.sort(thePeople, new ClassyComparator());
+
+		for (int i = 0; i < thePeople.size(); i++) {
 			System.out.println(thePeople.get(i).myName);
 		}
-		System.out.println();
-		
-		Collections.sort(thePeople, new ClassyComparator());
-		
-		for(int i = 0; i < thePeople.size(); i++) {
-			System.out.println("Name: " + thePeople.get(i).myName + "; the class: " + thePeople.get(i).myClass);
-		}
-		
-		
-		
 	}
-	
 }
